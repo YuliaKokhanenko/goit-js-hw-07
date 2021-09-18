238a57cbe55ca61b578d9a9e261b0635dc2a7a95
Yulia Kokhanenko <tarutina-z.julia@ukr.net>
2021-09-18 14:01:42 +0300

    Create task-01.js
    add task 1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const category = document.querySelector("#categories").childElementCount;
+const allli = document.querySelectorAll("#categories.item");
+console.log(`В списке $ {category} категории`);
+allli.forEach((item) => {
+  пункт
+    .querySelectorAll("h2")
+    .forEach((name) => console.log(`Категория: ${name.textContent}`)),
+    console.log(
+      `Количество элементов: $ {item.querySelectorAll('li'). length} `
+    );
+});
